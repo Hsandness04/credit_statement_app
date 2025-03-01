ba0aaedf0dde33400aee9f5ebbd685575832dbd0
Hunter Sandness <60943075+Hsandness04@users.noreply.github.com>
2025-03-01 13:26:32 -0600

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -3,19 +3,29 @@ from tkinter import ttk
 
 
 
-def display_transactions(frame, transactions_dict) -> None:
+def display_transactions(usbank_window, transactions_dict) -> None:
+    ttk.Button(usbank_window.frm, text="Quit", command=usbank_window.window.destroy).grid(column=1, row=11)
+
+    # Set column headings
+    ttk.Label(usbank_window.frm, text="Transaction Ref ID").grid(column=0, row=0)
+    ttk.Label(usbank_window.frm, text="Description").grid(column=1, row=0)
+    ttk.Label(usbank_window.frm, text="Amount").grid(column=2, row=0)
+    ttk.Label(usbank_window.frm, text="SubCategory").grid(column=3, row=0)
+
     row = 1
     for tran in transactions_dict:
         if row >= 11:
             break
-        ttk.Label(frame, text=tran).grid(column=0, row=row)
-        ttk.Label(frame, text=transactions_dict[tran]["details"]["description"]).grid(column=1, row=row)
-        ttk.Label(frame, text=transactions_dict[tran]["details"]["amount"]).grid(column=2, row=row)
-        ttk.Entry(frame).grid(column=3, row=row)
+        if transactions_dict[tran]["details"]["subcategory"] != "":
+            continue
+        ttk.Label(usbank_window.frm, text=tran).grid(column=0, row=row)
+        ttk.Label(usbank_window.frm, text=transactions_dict[tran]["details"]["description"]).grid(column=1, row=row)
+        ttk.Label(usbank_window.frm, text=transactions_dict[tran]["details"]["amount"]).grid(column=2, row=row)
+        ttk.Entry(usbank_window.frm).grid(column=3, row=row)
         row += 1
 
-# Iterate through all widgets in the frame and return an array that is the length
-# of how many valid entries there are i.e no space only entries.
+# Iterate through all widgets in the frame and return an array of
+# Entry widgets.
 def check_entries(frame) -> list:
     entries = []
     for widget in frame.winfo_children():
