f14cf0e221ee4ff39e658ea504aafdc41d1a9d5d
Hunter Sandness <60943075+Hsandness04@users.noreply.github.com>
2025-04-14 17:55:47 -0500

    Update transactions.py
    Create Transactions class for persistence and removal of deepcopy for transaction dictionary.

--- transactions.py
+++ transactions.py
@@ -1,50 +1,68 @@
 from tkinter import ttk
 
+class Transactions:
+
+    def __init__(self, window, transactions):
+        self.window = window
+        self.transactions: dict = transactions
+        self.entries = {}
+
+    def display_transactions(self) -> None:
+        # Adjust location of quit button depending on how many transactions
+        # remain in the dictionary.
+        if len(self.transactions.keys()) < 11:
+            row = len(self.transactions.keys()) + 1
+            ttk.Button(self.window.frm, text="Quit", command=self.window.window.destroy).grid(column=1, row=row)
+        else :
+            ttk.Button(self.window.frm, text="Quit", command=self.window.window.destroy).grid(column=1, row=11)
+
+        # Set column headings
+        self.display_transaction_headings()
+
+        # For each new 10 transactions, initialize an empty
+        # dictionary.
+        row = 1
+        self.entries = {}
+        for tran in self.transactions:
+            if row >= 11 or row > len(self.transactions.keys()):
+                break
+            tran_category = self.transactions[tran]["details"]["category"]
+            tran_subcategory = self.transactions[tran]["details"]["subcategory"]
+            if tran_category.strip() != "" or tran_subcategory.strip() != "":
+                continue
+
+            ttk.Label(self.window.frm, text=tran).grid(column=0, row=row)
+            ttk.Label(self.window.frm, text=self.transactions[tran]["details"]["description"]).grid(column=1, row=row)
+            ttk.Label(self.window.frm, text=self.transactions[tran]["details"]["amount"]).grid(column=2, row=row)
+
+            self.entries[tran] = {"category": "",
+                            "subcategory": ""}
+            # Add transaction category
+            category_entry = ttk.Entry(self.window.frm)
+            category_entry.grid(column=3, row=row, padx=10, pady=10)
+            self.entries[tran]["category"] = category_entry
+            # Add transaction subcategory
+            subcategory_entry = ttk.Entry(self.window.frm)
+            subcategory_entry.grid(column=4, row=row, padx=10, pady=10)
+            self.entries[tran]["subcategory"] = subcategory_entry
+
+            row += 1
+                
+
+    def display_transaction_headings(self) -> None:
+        # Set column headings
+        ttk.Label(self.window.frm, text="Transaction Ref ID").grid(column=0, row=0)
+        ttk.Label(self.window.frm, text="Description").grid(column=1, row=0)
+        ttk.Label(self.window.frm, text="Amount").grid(column=2, row=0)
+        ttk.Label(self.window.frm, text="Category").grid(column=3, row=0)
+        ttk.Label(self.window.frm, text="SubCategory").grid(column=4, row=0)
 
 
-
-def display_transactions(usbank_window, transactions_dict) -> dict:
-    # Adjust location of quit button depending on how many transactions
-    # remain in the dictionary.
-    if len(transactions_dict.keys()) < 11:
-        row = len(transactions_dict.keys()) + 1
-        ttk.Button(usbank_window.frm, text="Quit", command=usbank_window.window.destroy).grid(column=1, row=row)
-    else :
-        ttk.Button(usbank_window.frm, text="Quit", command=usbank_window.window.destroy).grid(column=1, row=11)
-
-    # Set column headings
-    ttk.Label(usbank_window.frm, text="Transaction Ref ID").grid(column=0, row=0)
-    ttk.Label(usbank_window.frm, text="Description").grid(column=1, row=0)
-    ttk.Label(usbank_window.frm, text="Amount").grid(column=2, row=0)
-    ttk.Label(usbank_window.frm, text="Category").grid(column=3, row=0)
-    ttk.Label(usbank_window.frm, text="SubCategory").grid(column=4, row=0)
-
-    row = 1
-    # For each new 10 transactions, initialize an empty
-    # dictionary.
-    entries = {}
-    for tran in transactions_dict:
-        if row >= 11 or row > len(transactions_dict.keys()):
-            break
-        if transactions_dict[tran]["details"]["subcategory"].strip() != "":
-            continue
-
-        ttk.Label(usbank_window.frm, text=tran).grid(column=0, row=row)
-        ttk.Label(usbank_window.frm, text=transactions_dict[tran]["details"]["description"]).grid(column=1, row=row)
-        ttk.Label(usbank_window.frm, text=transactions_dict[tran]["details"]["amount"]).grid(column=2, row=row)
-
-        # Initialize key in entries dictionary
-        entries[tran] = {"category": "",
-                         "subcategory": ""}
-        # Add transaction category
-        category_entry = ttk.Entry(usbank_window.frm)
-        category_entry.grid(column=3, row=row, padx=10, pady=10)
-        entries[tran]["category"] = category_entry
-        # Add transaction subcategory
-        subcategory_entry = ttk.Entry(usbank_window.frm)
-        subcategory_entry.grid(column=4, row=row, padx=10, pady=10)
-        entries[tran]["subcategory"] = subcategory_entry
-
-        row += 1
-
-    return entries
+    def redraw_transactions(self) -> None:
+        # Delete all non-header rows and redraw.
+        for widget in self.window.frm.winfo_children():
+            row = widget.grid_info()["row"]
+            if row == 0:
+                continue
+            widget.destroy()
+        self.display_transactions()
