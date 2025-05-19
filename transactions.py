b9230d8946e4f697444da1891604cf05f31f398a
Hunter Sandness <60943075+Hsandness04@users.noreply.github.com>
2025-05-19 09:26:36 -0500

    Update transactions.py
    Allow user to edit the description text of the transaction in case pypdf or in-app regular expressions didn't remove unwanted characters.

--- transactions.py
+++ transactions.py
@@ -1,5 +1,6 @@
 from tkinter import ttk
 import tkinter as tk
+from tkinter import messagebox
 
 class Transactions:
 
@@ -8,10 +9,22 @@ class Transactions:
         self.transactions: dict = transactions
         self.entries = {}
 
-
     def check_key_length(self) -> int:
         return len(self.transactions.keys())
     
+    def create_buttons(self) -> None:      
+        # Adjust location of quit button depending on how many transactions
+        # remain in the dictionary.
+        if self.check_key_length() < 11:
+            row = len(self.transactions.keys()) + 1
+            ttk.Button(self.window.frm, text="Quit", command=self.window.window.destroy).grid(column=1, row=row)
+            ttk.Button(self.window.frm, text="Submit", command=self.submit_entries).grid(column=3, row=row)
+            ttk.Button(self.window.frm, text="Delete Transactions", command=self.delete_entries).grid(column=5, row=row)
+        else :
+            ttk.Button(self.window.frm, text="Quit", command=self.window.window.destroy).grid(column=1, row=11)
+            ttk.Button(self.window.frm, text="Submit", command=self.submit_entries).grid(column=3, row=11)
+            ttk.Button(self.window.frm, text="Delete Transactions", command=self.delete_entries).grid(column=5, row=11)
+
     def check_cat_subcategory_fields(self, transaction) -> int: # 0 both fields aren't set
         tran_category = self.transactions[transaction]["details"]["category"]
         tran_subcategory = self.transactions[transaction]["details"]["subcategory"]
@@ -25,17 +38,8 @@ class Transactions:
         return set_fields
 
     def display_transactions(self) -> None:
-        # Adjust location of quit button depending on how many transactions
-        # remain in the dictionary.
-        if self.check_key_length() < 11:
-            row = len(self.transactions.keys()) + 1
-            ttk.Button(self.window.frm, text="Quit", command=self.window.window.destroy).grid(column=1, row=row)
-            ttk.Button(self.window.frm, text="Submit", command=self.window.window.destroy).grid(column=3, row=row)
-            ttk.Button(self.window.frm, text="Delete Transactions", command=self.window.window.destroy).grid(column=4, row=row)
-        else :
-            ttk.Button(self.window.frm, text="Quit", command=self.window.window.destroy).grid(column=1, row=11)
-            ttk.Button(self.window.frm, text="Submit", command=self.window.window.destroy).grid(column=3, row=row)
-            ttk.Button(self.window.frm, text="Delete Transactions", command=self.window.window.destroy).grid(column=4, row=row)
+
+        self.create_buttons()
 
         # Set column headings
         self.display_transaction_headings()
@@ -52,12 +56,17 @@ class Transactions:
                 continue
 
             ttk.Label(self.window.frm, text=tran).grid(column=0, row=row)
-            ttk.Label(self.window.frm, text=self.transactions[tran]["details"]["description"]).grid(column=1, row=row)
+            #ttk.Label(self.window.frm, text=self.transactions[tran]["details"]["description"]).grid(column=1, row=row)
             ttk.Label(self.window.frm, text=self.transactions[tran]["details"]["amount"]).grid(column=2, row=row)
 
             self.entries[tran] = {"category": "",
                             "subcategory": "",
                             "checkbox": {"widget": "", "checked": ""}}
+            # Add description and allow user to edit
+            description_entry = ttk.Entry(self.window.frm, width=60)
+            description_entry.grid(column=1, row=row, padx=10, pady=10)
+            description_entry.insert(0, self.transactions[tran]["details"]["description"])
+            self.entries[tran]["description"] = description_entry
             # Add transaction category
             category_entry = ttk.Entry(self.window.frm)
             category_entry.grid(column=3, row=row, padx=10, pady=10)
@@ -74,7 +83,6 @@ class Transactions:
             self.entries[tran]["checkbox"]["checked"] = checkbox_value
 
             row += 1
-                
 
     def display_transaction_headings(self) -> None:
         # Set column headings
@@ -85,7 +93,6 @@ class Transactions:
         ttk.Label(self.window.frm, text="SubCategory").grid(column=4, row=0)
         ttk.Label(self.window.frm, text="Remove Transaction").grid(column=5, row=0)
 
-
     def redraw_transactions(self) -> None:
         # Delete all non-header rows and redraw.
         for widget in self.window.frm.winfo_children():
@@ -94,3 +101,44 @@ class Transactions:
                 continue
             widget.destroy()
         self.display_transactions()
+        self.bind_entries()
+
+    def bind_entries(self):
+        for entry in self.entries:
+            self.entries[entry]["description"].bind("<Return>", self.submit_entries)
+            self.entries[entry]["category"].bind("<Return>", self.submit_entries)
+            self.entries[entry]["subcategory"].bind("<Return>", self.submit_entries)
+    
+    # event argument must be set to None. bind method automatically passes event object
+    # while method as a callback in the Button object does not pass any argument.
+    def submit_entries(self, event=None):
+        submissions = 0 # Keep track of the number of entries submitted.
+        for entry in self.entries:
+            # Delete entries on submit if user did not select delete checkbox.
+            if self.entries[entry]["checkbox"]["checked"].get() == 1:
+                self.delete_entry(entry)
+            if self.entries[entry]["category"].get().strip() != "" or self.entries[entry]["subcategory"].get().strip() != "":
+                submissions += 1 # Track the amout of entries being submitted.
+            if self.entries[entry]["category"].get().strip() != "":
+                self.transactions[entry]["details"]["category"] = self.entries[entry]["category"].get()
+            if self.entries[entry]["subcategory"].get().strip() != "":
+                self.transactions[entry]["details"]["subcategory"] = self.entries[entry]["subcategory"].get()
+            # No need for if check for description, user either makes edits or leaves it as is.
+            # Upload the description after the category and subcategory have been reviewed by the user.
+            self.transactions[entry]["details"]["description"] = self.entries[entry]["description"].get()
+
+        messagebox.showinfo("Successful Entries", f"{submissions} entries have been submitted!")
+
+        # Populate new transactions within the frame that have not 
+        # had their category or subcategory field filled out. Then bind the
+        # submit_entries function to the new entries.
+        self.redraw_transactions()
+
+    def delete_entry(self, entry_key) -> None:
+        del self.transactions[entry_key]
+
+    def delete_entries(self) -> None:
+        for entry in self.entries:
+            if self.entries[entry]["checkbox"]["checked"].get() == 1:
+                self.delete_entry(entry)
+        self.redraw_transactions()
