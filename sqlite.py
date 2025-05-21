5ba0c368d38787c3bce4a14999bb00dfe1a86db3
Hunter Sandness <60943075+Hsandness04@users.noreply.github.com>
2025-05-21 11:00:00 -0500

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -6,13 +6,13 @@ class SQLiteConnector:
         self.conn = sqlite3.connect(database)  # Creates the DB file if it doesn't exist
         self.cursor = self.conn.cursor()
 
+
     def create_table(self, table_name: str):
 
         query = f'''
             CREATE TABLE IF NOT EXISTS {table_name} (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 transaction_reference TEXT,
-                date TEXT,
                 description TEXT,
                 amount REAL,
                 category TEXT,
@@ -21,27 +21,34 @@ class SQLiteConnector:
         '''
         self.cursor.execute(query)
 
+
     def key_exists(self, key) -> bool:
-        self.cursor.execute("SELECT 1 FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
-        if self.cursor.fetchone() is not None:
+        query_result = self.cursor.execute("SELECT * FROM transactions WHERE transaction_reference = ?;", (key,))
+        value = query_result.fetchone()
+        if value is not None:
             return True
         False
+
 
     def dict_list_tuple_conversion(self, dict: dict) -> list[tuple]:
         data = []
         for key in dict:
-            trans_details = dict[key]["details"]
+            if dict[key]["delete"] == True:
+                self.delete(key)
+
+            transaction_details = dict[key]["details"]
             #   Check if category or subcategory fields are already set. If they are, only overwrite them
             #   if the new value is different than the existing one. If they aren't, don't overwrite existing
             #   values with blanks.
-            if trans_details["category"] is None or trans_details["category"] == "" and self.cateogry_exists(key):
-                trans_details[key]["details"]["category"] =  self.select_category(key) # Set new field to existing field, sinze new field is blank.
+            if transaction_details is None or transaction_details["category"] == "" and self.category_exists(key):
+                transaction_details[key]["details"]["category"] =  self.select_category(key) # Set new field to existing field, since new field is blank.
             
-            trans_tuple = (key, trans_details["amount"], trans_details["description"], 
-                    trans_details["category"], trans_details["subcategory"])
+            trans_tuple = (key, transaction_details["amount"], transaction_details["description"], 
+                    transaction_details["category"], transaction_details["subcategory"])
             data.append(trans_tuple)
         
         return data
+
 
     def insert_transactions(self, dict: dict) -> None:
         # Early return if there are zero transactions.
@@ -53,26 +60,22 @@ class SQLiteConnector:
         for key in dict:
             if self.key_exists(key):
                 existing_data[key] = {}
-                ###
-
                 # Check if category and subcategory submissions are blank. If they are,
                 # default to current existing category and subcategory fields.
-                ###
-                existing_data[key]["details"] = self.set_defaults_if_blank_entries(key, dict[key]["details"])
-                continue
-            new_data[key] = {}
-            new_data[key]["details"] = dict[key]["details"]
+                existing_data[key]["details"] = self.return_transaction_details(key, dict[key]["details"])
+                existing_data[key]["delete"] = dict[key]["delete"]
+            else:
+                new_data[key] = {}
+                new_data[key]["details"] = dict[key]["details"]
+                new_data[key]["delete"] = dict[key]["delete"]
 
         self.insert_existing_transactions(existing_data)
         self.insert_new_transactions(new_data)
         
-    def insert_existing_transactions(self, dict) -> None:
-        # Early return if no transactions exist yet.
-        if all(value == 0 for value in dict.values()):
-            return
-            
-        existing_data = self.dict_list_tuple_conversion(dict)
 
+    def insert_existing_transactions(self, dict) -> None:
+
+        existing_data = self.dict_list_tuple_conversion(dict)
         for transaction_ref, amount, description, category, subcategory in existing_data:
             self.cursor.execute('''
                     UPDATE transactions
@@ -83,6 +86,7 @@ class SQLiteConnector:
                     WHERE transaction_reference = ?
                 ''', (description, amount, category, subcategory, transaction_ref))    
         
+
     def insert_new_transactions(self, dict) -> None:
         new_data = self.dict_list_tuple_conversion(dict)
         self.cursor.executemany('''
@@ -90,38 +94,60 @@ class SQLiteConnector:
                 VALUES (?, ?, ?, ?, ?)
             ''', new_data)
         
-    def set_defaults_if_blank_entries(self, key, transaction_record:dict) -> dict:
+
+    def return_transaction_details(self, key, transaction_record:dict) -> dict:
         if transaction_record["category"].strip() == "":
             transaction_record["category"] = self.select_category(key)
         if transaction_record["subcategory"].strip() == "":
             transaction_record["subcategory"] = self.select_subcategory(key)
+        return transaction_record
         
-    def cateogry_exists(self, key) -> bool:
-        result = self.cursor.execute("SELECT category FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
-        if result is None:
+
+    def category_exists(self, key) -> bool:
+        query_result = self.cursor.execute("SELECT category FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
+        value = query_result.fetchone()
+        if value is None or value[0] == "":
             return False
-        True
+        return True
+
 
     def select_category(self, key) -> str:
-        self.cursor.execute("SELECT category FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
-        return self.cursor.fetchone()
+        if self.category_exists(key):
+            query_result = self.cursor.execute("SELECT category FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
+            value = query_result.fetchone()
+            return str(value[0])
+        else:
+            return str()
     
-    def subcateogry_exists(self, key) -> bool:
-        result = self.cursor.execute("SELECT subcategory FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
-        if result is None:
+
+    def subcategory_exists(self, key) -> bool:
+        query_result = self.cursor.execute("SELECT subcategory FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
+        value = query_result.fetchone()
+        if value is None or value[0] == "":
             return False
-        True
+        return True
+
 
     def select_subcategory(self, key) -> str:
-        self.cursor.execute("SELECT subcategory FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
-        return self.cursor.fetchone()
+        if self.subcategory_exists(key):
+            query_result = self.cursor.execute("SELECT subcategory FROM transactions WHERE transaction_reference = ? LIMIT 1;", (key,))
+            value = query_result.fetchone()
+            return str(value[0])
+        return str()
     
+
     def select_all(self):
         self.cursor.execute("SELECT * FROM transactions")
         return self.cursor.fetchall()
-                            
+    
+
+    def delete(self, key):
+        self.cursor.execute("DELETE FROM transactions WHERE transaction_reference = ?;", (key,))
+
+
     def submit_changes(self):
         self.conn.commit()
+
 
     def close_connection(self):
         self.conn.close()
