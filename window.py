87ac8d5e8225afd750d5b01aed852e3b14906418
Hunter Sandness <60943075+Hsandness04@users.noreply.github.com>
2025-06-26 16:54:11 -0500

    Update window.py

--- window.py
+++ window.py
@@ -1,25 +1,15 @@
+
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-from transactions import *
+
 
 class Window:
 
     def __init__(self):
         self.window = Tk()
         self.frm = ttk.Frame(self.window, padding=10)
-
-        # Create a window with 4 column headings for the bank transactions and
-        # enough rows for at least 10 transactions to be displayed.
         self.frm.grid()
 
-    def redraw_transactions(self, transactions):
-        for widget in self.frm.winfo_children():
-            row = widget.grid_info()["row"]
-            if row == 0:
-                continue
-            widget.destroy()
-        display_transactions(self, transactions)
-
-    def start_window(self):
-
+    def start_window(self) -> None:
         self.window.mainloop()
