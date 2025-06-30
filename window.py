9e16c650ba74bd9c3310d0f6d7fcf223d421bfea
Hunter Sandness <60943075+Hsandness04@users.noreply.github.com>
2025-06-30 12:42:29 -0500

    Update window.py

--- window.py
+++ window.py
@@ -3,13 +3,30 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
-
-class Window:
-
-    def __init__(self):
-        self.window = Tk()
-        self.frm = ttk.Frame(self.window, padding=10)
+class BaseWindow:
+    def create_frame(self) -> Frame:
+        self.frm = ttk.Frame(self, padding=10)
         self.frm.grid()
+        return self.frm
+    
+
+
+class Window(tk.Tk, BaseWindow):
+    def __init__(self):
+        super().__init__()
+
 
     def start_window(self) -> None:
-        self.window.mainloop()
+        self.mainloop()
+
+    
+    def create_top_level_window(self) -> tk.Toplevel:
+        window = TopLevelWindow(self)
+        window.grab_set()
+        return window
+    
+
+
+class TopLevelWindow(tk.Toplevel, BaseWindow):
+    def __init__(self, parent):
+        super().__init__(parent)
